cdbe4e7182a6f48a5a04cbdf48ba8e4696a45d79
Andrey Chizhov <Achizhovjob@yandex.ru>
2024-07-27 21:36:02 +0400

    👍 set DatabaseConfig

--- service/config/__init__.py
+++ service/config/__init__.py
@@ -3,11 +3,12 @@ from pydantic_settings import BaseSettings
 
 from ._api_config import ApiConfig
 from ._run_config import RunConfig
+from ._db_config import DatabaseConfig
 
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiConfig = ApiConfig()
-    pass
+    database: DatabaseConfig 
 
 
 settings = Settings()
