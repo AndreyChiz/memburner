81b082843f612e137d2162c3a035c5f09488de01
Andrey Chizhov <Achizhovjob@yandex.ru>
2024-07-28 16:58:57 +0400

    🫰

--- service/main.py
+++ service/main.py
@@ -22,6 +22,7 @@ main_app.include_router(
     prefix=settings.api.prefix,
 )
 
+
 if __name__ == "__main__":
     uvicorn.run(
         "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
