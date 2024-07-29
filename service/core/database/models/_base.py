5611148f7a5cb73b3ac16c1fb2f321726b2710e0
Andrey Chizhov <Achizhovjob@yandex.ru>
2024-07-29 13:06:06 +0400

    👍 add camel_case_to_snake_case

--- service/core/database/models/_base.py
+++ service/core/database/models/_base.py
@@ -7,10 +7,26 @@ from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
 from typing import Any, Annotated
 from config import settings
 
+
 idpk = Annotated[
     uuid.UUID, mapped_column(primary_key=True, server_default=text("gen_random_uuid()"))
 ]
 
+
+def camel_case_to_snake_case(input_str: str) -> str:
+
+    chars = []
+    for c_idx, char in enumerate(input_str):
+        if c_idx and char.isupper():
+            nxt_idx = c_idx + 1
+            flag = nxt_idx >= len(input_str) or input_str[nxt_idx].isupper()
+            prev_char = input_str[c_idx - 1]
+            if prev_char.isupper() and flag:
+                pass
+            else:
+                chars.append("_")
+        chars.append(char.lower())
+    return "".join(chars)
 
 
 class Base(DeclarativeBase):
@@ -49,7 +65,7 @@ class Base(DeclarativeBase):
 
     @declared_attr
     def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+        return camel_case_to_snake_case(cls.__name__)
 
     def __repr__(self) -> str:
         values = ", ".join(
