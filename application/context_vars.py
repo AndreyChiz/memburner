d081038885212c121bb771317faa652cd9e6fddd
Andrey Chizhov <Achizhovjob@yandex.ru>
2024-11-14 19:41:59 +0400

    add doc

--- application/context_vars.py
+++ application/context_vars.py
@@ -1,6 +1,8 @@
 import contextvars
 from config import settings
 
+"just for logging, one common request_id"
+
 request_id_var = contextvars.ContextVar(
     "request_id",
     default=settings.app_name,
